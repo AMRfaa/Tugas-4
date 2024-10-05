8bc20a18e51360aa53ae639301fb1bd5a0f1b87d
Anisa Muziya Rafa <amrxll08@gmail.com>
2024-10-05 20:03:00 +0800

    Create tugas4ASD.py

---
+++ tugas4ASD.py
@@ -0,0 +1,42 @@
+# inisisasi matriks
+A = [[1,2,3],
+     [3,2,1],
+     [4,1,4]] 
+B = [[3,2,1],
+     [4,5,6],
+     [3,1,2]]
+
+# inisiasi hasil perkalian matriks
+C = [[0,0,0],[0,0,0],[0,0,0]] 
+
+# mengetahui baris dan kolom
+Baris_A = len(A)
+Kolom_A = len(A[0])
+
+Baris_B = len(B)
+Kolom_B = len(B[0])
+
+# Cetak detail matriks
+print (A)
+print("Baris matriks A adalah ", Baris_A)
+print("Kolom matriks A adalah ", Kolom_A)
+
+print (B)
+print("Baris matriks B adalah ", Baris_B)
+print("Kolom matriks B adalah ", Kolom_B)
+
+# cek kondisi Matrix untuk Perkalian
+if (Kolom_A != Baris_B):
+    print ("Matriks A dengan B tidak dapat dikalikan")
+else:
+     print ("Matriks A dengan B dapat dikalikan dan hasilnya adalah: ")
+for i in range(3):
+    for j in range(3): 
+        for k in range(3):  
+            C[i][j] += A[i][k] * B[k][j]
+          
+# output hasil perkalian matriks
+print("Hasil perkalian matriks A dan B:")
+for baris in C:
+    print(baris)    
+   
